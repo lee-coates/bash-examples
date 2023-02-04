afc403aa68a2502fcaf17e3512f5ce241154b4a3
Lee Coates <lee.coates.npm@outlook.com>
2023-02-03 21:37:13 -0800

    initial commit with a simple scp example

---
+++ working-with-ssh/scp.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# These commands are useful for copying files over ssh (scp)
+
+# Assuming your scp user has permissions to the target location, a simple scp takes the form:
+scp [path/source/file] [user_name@target_host:target/path]
+
